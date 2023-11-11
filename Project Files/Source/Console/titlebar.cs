9f04f806c04779b9dc5093b7d73b2be79bc3401b
mi0bot <mi0bot@trom.uk>
2023-11-11 14:47:42 +0000

    Latest 2.10.3.4 updates

--- Project Files/Source/Console/titlebar.cs
+++ Project Files/Source/Console/titlebar.cs
@@ -38,7 +38,7 @@ namespace Thetis
 
         public static string GetString(bool bWithFirmware = true)
         {
-            string sRevision = "." + Common.GetRevision(); //MW0LGE_22b
+            string sRevision = "." + Common.GetRevision();
             if (sRevision == ".0") sRevision = "";
 
             string version = Common.GetVerNum() + sRevision;
@@ -47,8 +47,7 @@ namespace Thetis
             string sBits = Common.Is64Bit ? " x64" : " x86";
 
             s += " v" + version + sBits;
-            //if (BUILD_DATE != "") s += " " + BUILD_DATE;
-            s += " (" + VersionInfo.BuildDate + ")<FW>";  //[2.10.2.2]MW0LGE use the audo generated class from pre build event for the BuildDate
+            s += " (" + VersionInfo.BuildDate + ")<FW>";  //[2.10.2.2]MW0LGE use the auto generated class from pre build event for the BuildDate
 
             if (BUILD_NAME != "") s += " " + BUILD_NAME;
 
