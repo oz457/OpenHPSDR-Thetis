d4fc0ad37c10939638549d306f307afdd22be4ac
mi0bot <mi0bot@trom.uk>
2024-07-31 07:13:38 +0100

    0d27471 - Updated from official codebase

--- Project Files/Source/Console/console.cs
+++ Project Files/Source/Console/console.cs
@@ -15381,8 +15381,8 @@ namespace Thetis
             lock (m_objCatParseLocker)
             {
                 sRet = m_objTCPIPCatParser.Get(msg);
-            }
-            return sRet;
+                return sRet;
+            }            
         }
 
         public bool _bSendLimitedPowerLevels = false;
