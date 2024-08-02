37ee5fc8ebd9447d73d4d6a2daada6722b5b2642
mi0bot <mi0bot@trom.uk>
2024-08-02 08:22:32 +0100

    45af61d - Updated from official codebase

--- Project Files/Source/Console/VersionInfo.cs
+++ Project Files/Source/Console/VersionInfo.cs
@@ -6,6 +6,6 @@ namespace Thetis
 { 
     public static class VersionInfo 
     { 
-        public static string BuildDate { get { return "08/01/24"; } }  
+        public static string BuildDate { get { return "08/02/24"; } }  
     } 
 } 
